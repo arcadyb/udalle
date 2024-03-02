9452f36d2d2f7f8d084cc122a4023433c4123043
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2024-03-02 20:59:56 +0200

    create prompt

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 
-console.log("gid="+process.env.GOOGLE_ID)
+//console.log("gid="+process.env.GOOGLE_ID)
 
 const handler = NextAuth({
   providers: [
@@ -16,7 +16,7 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      console.log(session)
+      // console.log(session)
       // store the user id from MongoDB to session
       const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
