662cd84285753983b2649716d56dbb993d50f334
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2024-03-24 21:57:38 +0200

    loading

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    
+    appDir: true, 
     serverComponentsExternalPackages: ["mongoose"],
+   // serverComponentsExternalPackages: ["mongoose"],
     missingSuspenseWithCSRBailout: false,
   },
   images: {
